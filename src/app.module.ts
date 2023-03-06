53f6887bb38cfe48fee20d24e6345eff21c3d807
kelvinlee0225 <leekelvin0225@gmail.com>
2023-03-06 16:51:34 -0400

    create datasource and implement dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
 import { ConfigModule } from '@nestjs/config';
+import { dataSourceOptions } from './database/data-source';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.TYPEORM_HOST,
-      port: +process.env.TYPEORM_PORT,
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      entities: [],
-      synchronize: true, //development only
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     UsersModule,
   ],
 })
---
+++ src/database/data-source.ts
@@ -0,0 +1,19 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: +process.env.TYPEORM_HOST,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/database/migrations/*.js'],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;

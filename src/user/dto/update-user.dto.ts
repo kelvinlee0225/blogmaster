832df64e67008a8aecc99789c123296e6e1b1bbc
kelvinlee0225 <leekelvin0225@gmail.com>
2023-03-16 12:01:43 -0400

    implement validation decorators in user's dtos

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 import { UserDto } from './user.dto';
 
 export class UpdateUserDto extends PartialType(UserDto) {
+  @IsUUID()
+  @IsNotEmpty()
   id: string;
 }
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,8 +1,20 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { UserType } from '../enums/user-type-enum';
 
 export class UserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsEnum(UserType)
+  @IsNotEmpty()
   userType: UserType;
 }

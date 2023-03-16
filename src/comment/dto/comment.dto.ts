d14f0c4d4a909a26115960a28ef291333e0c9e57
kelvinlee0225 <leekelvin0225@gmail.com>
2023-03-16 12:58:56 -0400

    implement class validator in comment's dtos

--- src/comment/dto/comment.dto.ts
+++ src/comment/dto/comment.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+
 export class CommentDto {
+  @IsString()
+  @IsNotEmpty()
   body: string;
+
+  @IsUUID()
+  @IsOptional()
   parentId: string;
+
+  @IsUUID()
+  @IsNotEmpty({ message: 'user id is required' })
   userId: string;
+
+  @IsUUID()
+  @IsNotEmpty({ message: 'blog post id is required' })
   blogPostId: string;
 }
--- src/comment/dto/update-comment.dto.ts
+++ src/comment/dto/update-comment.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CommentDto } from './comment.dto';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class UpdateCommentDto extends PartialType(CommentDto) {
+  @IsUUID()
+  @IsNotEmpty()
   id: string;
 }

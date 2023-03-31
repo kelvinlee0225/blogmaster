2288134adb1a466b94e24b9ad0acc4f594fc622b
kelvinlee0225 <leekelvin0225@gmail.com>
2023-03-31 12:59:03 -0400

    Implement Swagger to blogpost's controller and dots

--- src/blogpost/dto/blogpost.dto.ts
+++ src/blogpost/dto/blogpost.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class BlogPostDto {
+  @ApiProperty({ type: String, required: true })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ type: String, required: true })
   @IsString()
   @IsNotEmpty()
   body: string;
 
+  @ApiProperty({ type: String, required: true })
   @IsUUID()
   @IsNotEmpty()
   userId: string;

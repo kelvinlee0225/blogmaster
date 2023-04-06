b6028f266fbc26bd456912ae6e947d5962aab056
kelvinlee0225 <leekelvin0225@gmail.com>
2023-04-06 17:04:39 -0400

    apply @golevelup/ts-jest to create repository mock

--- src/common/mock-data/repository.mock.ts
+++ src/common/mock-data/repository.mock.ts
@@ -1,6 +1,21 @@
-export const repositoryMock = {
-  findOneOrFail: jest.fn(() => ({})),
-  find: jest.fn(() => ({})),
-  save: jest.fn(() => ({})),
-  remove: jest.fn(() => ({})),
-};
+import { createMock } from '@golevelup/ts-jest';
+import { Repository } from 'typeorm';
+
+export const repositoryMock = createMock<Repository<any>>({
+  findOneOrFail: jest.fn().mockResolvedValue({}),
+  findOneByOrFail: jest.fn().mockResolvedValue({}),
+  find: jest.fn().mockResolvedValue({}),
+  save: jest.fn().mockResolvedValue({}),
+  remove: jest.fn().mockResolvedValue({}),
+  softDelete: jest.fn().mockResolvedValue({}),
+  softRemove: jest.fn().mockResolvedValue({}),
+  createQueryBuilder: jest.fn().mockResolvedValue({
+    where: jest.fn().mockResolvedValue({}),
+    andWhere: jest.fn().mockReturnThis(),
+    orderBy: jest.fn().mockReturnThis(),
+    skip: jest.fn().mockReturnThis(),
+    take: jest.fn().mockReturnThis(),
+    limit: jest.fn().mockReturnThis(),
+    offset: jest.fn().mockReturnThis(),
+  }),
+});

e17771859c1eba0a9d73bbfcfc881f30858948c4
kelvinlee0225 <leekelvin0225@gmail.com>
2023-04-13 16:40:11 -0400

    add displayRequestDuration to swagger UI

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,9 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: { displayRequestDuration: true },
+  });
 
   await app.listen(3000);
 }
